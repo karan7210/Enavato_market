4672ea9e8411319264fcab272e170a4b0a859e8c
karan kumar <karankumar.971750@gmail.com>
2024-05-14 15:10:44 +0530

    done

---
+++ script.js
@@ -0,0 +1,41 @@
+let dropdown=document.getElementById("dropdown");
+let down=document.getElementById("down");
+dropdown.addEventListener("mouseenter",function(){
+    down.style.visibility="visible"
+})
+
+dropdown.addEventListener("mouseleave",function(){
+    down.style.visibility="hidden"
+})
+
+
+down.addEventListener("mouseenter",function(){
+    down.style.visibility="visible"
+})
+
+down.addEventListener("mouseleave",function(){
+    down.style.visibility="hidden"
+})
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
+let dropdown_page=document.getElementById("dropdown_page");
+let down_page=document.getElementById("down_page");
+dropdown_page.addEventListener("mouseenter",function(){
+    down_page.style.visibility="visible"
+})
+
+dropdown_page.addEventListener("mouseleave",function(){
+    down_page.style.visibility="hidden"
+})
+
+
+down_page.addEventListener("mouseenter",function(){
+    down_page.style.visibility="visible"
+})
+
+down_page.addEventListener("mouseleave",function(){
+    down_page.style.visibility="hidden"
+})
